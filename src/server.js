efc6015e9ed0ab6ef2f1a5e161280d39fdd11523
sandrodonizeti <129443003+sandrodonizeti@users.noreply.github.com>
2024-03-10 17:20:43 -0300

    Update server.js
    comite 10-03-2024

--- src/server.js
+++ src/server.js
@@ -1,30 +1,44 @@
-//const express = require('express')
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import userRouter from './routers/userRouter.js'
-import chatRouter from './routers/chatRouter.js'
-import produtoRouter from './routers/produtoRouter.js'
-import servicoRouter from './routers/servicoRouter.js'
-import authRouter from './routers/authRouter.js'
-import {PORT} from './config.js' 
+// Importando os módulos necessários
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import userRouter from './routers/userRouter.js';
+import chatRouter from './routers/chatRouter.js';
+import produtoRouter from './routers/produtoRouter.js';
+import servicoRouter from './routers/servicoRouter.js';
+import authRouter from './routers/authRouter.js';
+import { PORT } from './config.js';
 
+// Criando a instância do Express
 const api = express();
-const server = http.createServer(api); // Crie o servidor HTTP a partir do Express  
-const io = new Server(server); // Crie o servidor Socket.io a partir do servidor HTTP
+const server = http.createServer(api); // Criando o servidor HTTP a partir do Express
+const io = new Server(server); // Criando o servidor Socket.io a partir do servidor HTTP
 
-
-var corsOptions = {
-    credentials: true,
-    origin: ['http://localhost:3000', 'https://react-vivenciando.onrender', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173']
-    }
+// Configurando as opções CORS
+const corsOptions = {
+  credentials: true,
+  origin: [
+    'http://localhost:3000',
+    'https://react-vivenciando.onrender',
+    'http://127.0.0.1:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+  ],
+};
 
 // Habilitando o CORS com as opções configuradas
 api.use(cors(corsOptions));
 
+// Configurando middlewares
+api.use(bodyParser.json());
+api.use(cookieParser());
+
+// Rotas
 api.get('/', (req, res) => {
-  res.json({ message: "Bem-vindo a nossa API" });
+  res.json({ message: 'Bem-vindo a nossa API' });
 });
 
 api.use('/user', userRouter);
@@ -33,21 +47,23 @@ api.use('/chat', chatRouter);
 api.use('/produto', produtoRouter);
 api.use('/servico', servicoRouter);
 
+// Eventos do Socket.io
 io.on('connection', (socket) => {
-    console.log('Novo cliente conectado');
-  
-    socket.on('sendMessage', (data) => {
-      // Lógica para processar a nova mensagem recebida do cliente
-      // Aqui você pode emitir a mensagem para todos os clientes conectados
-      io.emit('newMessage', { chat: data }); // Envia a mensagem de volta para todos os clientes
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('Cliente desconectado');
-    });
-  });
-  
+  console.log('Novo cliente conectado');
 
-api.listen(PORT, () => {
+  socket.on('sendMessage', (data) => {
+    // Lógica para processar a nova mensagem recebida do cliente
+    // Aqui você pode emitir a mensagem para todos os clientes conectados
+    io.emit('newMessage', { chat: data }); // Envia a mensagem de volta para todos os clientes
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
+
+// Iniciando o servidor
+server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}! http://localhost:${PORT}`);
 });
+
